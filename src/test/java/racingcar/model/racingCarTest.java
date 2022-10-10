420b7da591498098babc3bd98e99b9f98ad97084
shchon <shchon1993@naver.com>
2022-10-10 16:52:21 +0900

    test: racingcarMoveCountTest.java, racingcarNameTest.java, racingcarPositionTest.java(자동차_여러대_포지션_값_음수()), racingcarsNameTest.java, racingCarsTest.java, racingCarTest.java
    refactor:
    *테스트코드 리팩토링 및 추가 테스트코드 작성

--- src/test/java/racingcar/model/racingCarTest.java
+++ src/test/java/racingcar/model/racingCarTest.java
@@ -3,10 +3,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 public class racingCarTest {
-
     @Test
     void 자동차_1대_출발선_테스트(){
-        racingCar car = new racingCar(new racingcarName("pobi"), new racingcarPosition(0));
+        racingCar car = new racingCar(new racingcarName("pobi"));
         assertThat(car.getCarNameClass().getCarName()).isEqualTo("pobi");
         assertThat(car.getPositionClass().getPosition()).isEqualTo(0);
 
