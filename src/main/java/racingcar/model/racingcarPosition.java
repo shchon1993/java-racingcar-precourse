8a185a6a0e810e1b29f0d40e848c24df2a836f39
shchon <shchon1993@naver.com>
2022-10-10 16:58:09 +0900

    add: racingcarPosition.java (void move()) * position 증가 method 변경

--- src/main/java/racingcar/model/racingcarPosition.java
+++ src/main/java/racingcar/model/racingcarPosition.java
@@ -4,9 +4,6 @@ import racingcar.utils.exceptionMessage;
 
 public class racingcarPosition {
     private exceptionMessage message = new exceptionMessage();
-
-
-
     private int position;
     private final String Move_position = "-";
     private final int moveCount = 1;
@@ -14,26 +11,16 @@ public class racingcarPosition {
         validationcheckPositionNegative(position);
         this.position = position;
     }
-
     public void validationcheckPositionNegative(int position){
         if(position < 0)
-            throw new IllegalArgumentException(message.InputNumNegative());
+            throw new IllegalStateException(message.WrongPositionNumber());
     }
-
     public int getPosition() {
         return position;
     }
-
-    /* public racingcarPosition move(){
-        System.out.println("move(): " + (position + moveCount));
-        return new racingcarPosition((position + moveCount));
+     public void move(){
+         this.position = position + moveCount;
     }
-
-    */
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
     @Override
     public String toString() {
         if(position == 0) return "";
